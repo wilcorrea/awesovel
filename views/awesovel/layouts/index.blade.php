e29cd514780f927a26c2928a740ad9335ef604fb
Pedro Mázala <pedroh.mazala@gmail.com>
2016-01-15 10:24:26 -0200

    Diretório modificado para views

--- views/awesovel/layouts/index.blade.php
+++ views/awesovel/layouts/index.blade.php
@@ -4,6 +4,11 @@
     <div class="container">
         <h3>{{ $operation->title }}</h3>
         <div class="jumbotron">
+            <table class="table">
+                <thead>
+                <th></th>
+                </thead>
+            </table>
             @foreach($data as $value)
                 <p>{{ $value }}</p>
             @endforeach
